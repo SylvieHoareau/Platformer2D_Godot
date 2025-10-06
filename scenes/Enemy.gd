d6ca8d940f9ec03b40823b7bb8ec7b4ff9673fe4
Sylvie HOAREAU <hoareausms@gmail.com>
2025-10-06 15:54:55 +0400

    fix: correction du script de l'ennemi

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -23,9 +23,24 @@ var target_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	$NavigationAgent2D.target_position = Goal.global_position
-	#
+	
+	await get_tree().physics_frame
+	
+	# Initialise la cible sur le premier point
+	set_new_target(WAYPOINTS[current_waypoint_index])
 	
 func _physics_process(delta: float) -> void:
+	# Vérifie si l'ennemi a atteint sa navigation
+	if navigation_agent.is_navigation_finished():
+		# Passe au point de cheminement suivant :
+		current_waypoint_index = (current_waypoint_index + 1) % WAYPOINTS.size()
+		# Définit la nouvelle cible
+		set_new_target(WAYPOINTS[current_waypoint_index])
+	
+	# Calcule la direction de mouvement
+	var next_point: Vector2 = navigation_agent.get_next_path_position()
+	var direction: Vector2 = global_position.direction_to(next_point)
+	
 	if !$NavigationAgent2D.is_target_reached():
 		var nav_point_direction = to_local($NavigationAgent2D.get_next_path_position()).normalized()
 		velocity = nav_point_direction * movement_speed * delta	
